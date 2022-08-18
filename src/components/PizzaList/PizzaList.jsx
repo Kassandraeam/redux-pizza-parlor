f4862ceaf8dba916f433bd20e88917db0a0522d8
Madeline Prickette <madelineprickette@gmail.com>
2022-08-18 14:53:25 -0500

    completed pizza list and added 2 reducers

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,6 +1,20 @@
+import {useSelector} from 'react-redux';
+import PizzaItem from '../PizzaItem/PizzaItem.jsx';
+
 function PizzaList() {
+
+    //ASKING STORE FOR THE PIZZA LIST
+    const pizzaList = useSelector(store => store.pizzaList);
+
     return(
-        <h3>Pizza List goes here...</h3>
+        <>
+            <h3>Pizza List goes here...</h3>
+            <ul>
+                {pizzaList.map((pizza) => 
+                    <PizzaItem key={pizza.id} pizza={pizza}/>
+                )}
+            </ul>
+        </>
     )
 }
 
